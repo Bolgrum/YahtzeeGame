ce8bd0e7859d80af5a282cd8a4ab2bd00e79f8b5
tnw012 <trevin_wolfe@hotmail.com>
2023-01-26 11:56:23 -0600

    changed rewrote generate_dice_role() and added a tests folder to root

--- yahtzee/generator.py
+++ yahtzee/generator.py
@@ -1,14 +1,10 @@
 import random
 
 def generate_dice_roll() -> list:
-    random_die1 = random.randint(1,6)
-    random_die2 = random.randint(1,6)
-    random_die3 = random.randint(1,6)
-    random_die4 = random.randint(1,6)
-    random_die5 = random.randint(1,6)
-    random_die6 = random.randint(1,6)
+    rolled_dice = list()
     
-    rolled_dice = [random_die1, random_die2, random_die3, random_die4, random_die5, random_die6,]
+    for random_number in range(1, 6):
+        random_dice_roll = random.randint(1, 6)
+        rolled_dice.append(random_dice_roll)
     
     return rolled_dice
-    

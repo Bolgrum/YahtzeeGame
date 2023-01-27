c93acbea1acfbd3cbae13071df212d289be408ea
tnw012 <trevin_wolfe@hotmail.com>
2023-01-27 14:45:30 -0600

    more modifications

--- yahtzee/main.py
+++ yahtzee/main.py
@@ -1,7 +1,14 @@
-import generator
+import generator, output
 
 if __name__ == "__main__":
-    #print('hello friend')
+    app = {
+        'name':'SE Yahtzee',
+        'author':["Anthony", "Spencer", "Trevin"],
+        'license':'MIT',
+        'version':"0.0.0"
+    }
+    
+    output.print_project_header(app)
     dice = generator.generate_dice_roll()
     
     print(dice)
--- yahtzee/output.py
+++ yahtzee/output.py
@@ -28,3 +28,33 @@ def print_project_header(app_info, border="="*50, intro_message="Help Message Go
     
 def print_rules():
     pass
+def print_project_header(app_info, border="="*50, intro_message="Help Message Goes Here", ):
+    """
+    ------------------------------------------------------------------------------
+    Function: print_project_header
+    ------------------------------------------------------------------------------
+    Description: This function prints to console the projects header that contains
+    data about the project
+    ------------------------------------------------------------------------------
+    """
+    # Prints the box containing information about the application
+    print(border)    
+    if type(app_info.get("author")) == list:
+        print(
+            "| App       : {:<9}|\n".format(app_info.get("name")) +
+            "| Author(s) : {:<9}|\n".format(str(app_info.get("author")).strip("[]")) +
+            "| Version   : {:<9}|\n".format(app_info.get("version")) +
+            "| License   : {:<9}|".format(app_info.get("license"))
+        )
+    else:
+        print(
+            "| App       : {:<9}|\n".format(app_info.get("name")) +
+            "| Author(s) : {:<9}|\n".format(app_info.get("author")) +
+            "| Version   : {:<9}|\n".format(app_info.get("version")) +
+            "| License   : {:<9}|".format(app_info.get("license"))
+        )
+    print(border)
+    print(intro_message + '\n')
+    
+def print_rules():
+    pass

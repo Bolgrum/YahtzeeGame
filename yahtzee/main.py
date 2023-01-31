e574945e299d1d9d26cc769402751ea0bc0db78f
tnw012 <trevin_wolfe@hotmail.com>
2023-01-31 14:07:19 -0600

    added scoring logic in scoring.py and removed some code from main that is not needed

--- yahtzee/main.py
+++ yahtzee/main.py
@@ -7,9 +7,8 @@ import yahtzee
 # Run main program
 if __name__ == "__main__":    
     if len(sys.argv) > 1:
-        # Used for command line entry ex. python main.py <arg>
-        if sys.argv[1].lower() == 'gui':
-            yahtzee.run_gui()
-            
+        # Used for command line entry ex. python main.py <arg>            
         if sys.argv[1].lower() == "console":
             yahtzee.run_console()
+    else:
+        yahtzee.run_gui()
